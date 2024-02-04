50c8f5ef92d0780226a8fbaec7caab258be9c3df
Ron Snyder <ron@snyderjr.com>
2024-02-04 17:52:01 -0500

    testing

--- v1/VisualEssay.vue
+++ v1/VisualEssay.vue
@@ -85,10 +85,11 @@ module.exports = {
 
           let sortedVisible = Object.values(visible).sort((a,b) => b.intersectionRatio - a.intersectionRatio || a.top - b.top)
           // sortedVisible.forEach(v => console.log(v.para, v.intersectionRatio, v.top))
-          console.log(sortedVisible.map(v => console.log(v.para.id, v.intersectionRatio, v.top)))
+          console.log(sortedVisible.map(v => console.log(v.para, v.intersectionRatio, v.top)))
 
           if (self.active !== sortedVisible[0]?.para.id) {
             self.active = sortedVisible[0]?.para.id
+            console.log(`active: ${self.active}`)
             document.querySelectorAll('.active').forEach(p => p.classList.remove('active'))
             sortedVisible[0]?.para?.classList.add('active')
             self.$emit('set-active', self.active)

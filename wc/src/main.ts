be1809e5bd22437a49cf04224be93aff9b413153
Ron Snyder <ron@snyderjr.com>
2024-03-06 09:31:32 -0500

    updated sl imports

--- wc/src/main.ts
+++ wc/src/main.ts
@@ -3,6 +3,7 @@ import ('preline')
 
 import '@shoelace-style/shoelace/dist/components/button/button.js'
 import '@shoelace-style/shoelace/dist/components/dropdown/dropdown.js'
+import '@shoelace-style/shoelace/dist/components/icon/icon.js'
 import '@shoelace-style/shoelace/dist/components/menu/menu.js'
 import '@shoelace-style/shoelace/dist/components/menu-item/menu-item.js'
 import '@shoelace-style/shoelace/dist/components/spinner/spinner.js'

0926d7c3104f5298f0c66425b93b42790e2d94ba
Ron Snyder <ron@snyderjr.com>
2024-03-06 11:25:03 -0500

    orientation detect

--- wc/src/components/Gallery.ce.vue
+++ wc/src/components/Gallery.ce.vue
@@ -45,7 +45,7 @@
     console.log(toRaw(manifests))
     images.value = manifests.map((manifest:any) => {
       let imgInfo = findItem({type:'Annotation', motivation:'painting'}, manifest, 1).body
-      let orientation = manifest.metadata?.find((item:any) => (item.label.en || item.label.none === 'orientation')).map(item => item.value.en || item.value.none)?.[0] || 1
+      let orientation = manifest.metadata?.filter((item:any) => (item.label.en || item.label.none === 'orientation')).map(item => item.value.en || item.value.none)[0] || 1
       return {
         id: manifest.id,
         label: manifest.label,

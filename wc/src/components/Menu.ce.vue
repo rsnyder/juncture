3d194d5363bb0cc8139f1990f7fc27b57755c754
Ron Snyder <ron@snyderjr.com>
2024-06-10 08:13:17 -0400

    testing

--- wc/src/components/Menu.ce.vue
+++ wc/src/components/Menu.ce.vue
@@ -137,19 +137,27 @@
         ? 'search'
         : action
     if (action === 'contact') mailto()
+    // else if (action === 'home')
+    //   location.href = item.href
     else if (action === 'search')
       window.open(item.href, '_blank')
     else {
       let href = new URL(item.href)
       let config = (window as any)?.config || {}
+      console.log('menuItemSelected', href)
+      console.log('location', location)
+      console.log('config', config)
       if (href.origin === location.origin) {
         let baseurl = config.baseurl || ''
-        let source = new URL(location.href).searchParams.get('source')?.split('/').filter(pe => pe) || []
-        if (source.length > 2) {
-          let [owner, repository, branch, path] = source
-          baseurl = `/${owner}/${repository}/${branch}`
+        if (href.pathname === location.pathname) {
+          let [owner, repository, branch, path] = new URL(location.href).searchParams.get('source')?.split('/').filter(pe => pe) || []
+          console.log('source', source)
+          if (owner && repository && branch) baseurl = `/${owner}/${repository}/${branch}`
         }
-        location.pathname = `${baseurl}${href.pathname}`
+        console.log('baseurl', baseurl)
+        let path = `${baseurl}${href.pathname}`
+        console.log('path', path)
+        // location.pathname = path
       } else {
         location.href = item.href
       }

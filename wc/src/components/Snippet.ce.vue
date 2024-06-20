f9a617cd15eaca4b2dd25050f536973fd84f4039
Ron Snyder <ron@snyderjr.com>
2024-06-20 14:55:19 -0400

    testing

--- wc/src/components/Snippet.ce.vue
+++ wc/src/components/Snippet.ce.vue
@@ -3,8 +3,8 @@
   <div ref="root" id="main">
     <sl-details v-if="collapsible" class="custom-icons" :open="props.open" :draggable="props.disableDrag ? null : ''" @dragstart="onDrag">
       
-      <sl-icon name="plus-square" slot="expand-icon"></sl-icon>
-      <sl-icon name="dash-square" slot="collapse-icon"></sl-icon>
+      <svg slot="expand-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg>
+      <svg slot="collapse-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-dash-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/><path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/></svg>
 
       <div slot="summary">
         <sl-tooltip content="Show code snippets" placement="top">

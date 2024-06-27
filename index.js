6c812048b0392b353af1324f5a13297acbf1eb93
Ron Snyder <ron@snyderjr.com>
2024-06-27 13:11:09 -0400

    testing

--- index.js
+++ index.js
@@ -1439,6 +1439,10 @@ function setViewersPosition() {
 }
 
 function mount(root, html) {  
+  if (window.config?.source?.path && !window.config?.source.dir) {
+    let pathElems = window.config.source.path.split('/').filter(pe => pe)
+    window.config.source.dir = `/source/${pathElems.slice(0,-1).join('/')}`
+  }
   window.config = {...yaml.parse(window.options || ''), ...(window.jekyll || {}), ...(window.config || {})}
   setMeta()
   root = root || document.body.querySelector('main')
